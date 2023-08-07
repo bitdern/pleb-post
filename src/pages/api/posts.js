236660f80cf9145ff7375c088bfd1559ec6c8459
aidan.newell <aidan.newell@river.com>
2023-08-07 07:29:50 -0500

    get / add api for posts

---
+++ src/pages/api/posts.js
@@ -0,0 +1,40 @@
+import connectMongo from "@/lib/connectMongo";
+import Posts from "@/models/Posts";
+
+export default function handler(req, res) {
+  switch (req.method) {
+    case "GET": {
+      return getPosts(req, res);
+    }
+    case "POST": {
+      return addPost(req, res);
+    }
+    default: {
+      return res.status(405).json({ error: "Method not allowed" });
+    }
+  }
+}
+
+async function getPosts(req, res) {
+  try {
+    await connectMongo();
+
+    const posts = await Posts.find();
+
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+async function addPost(req, res) {
+  try {
+    await connectMongo();
+
+    const post = await Posts.create(req.body);
+
+    res.status(201).json(post);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
