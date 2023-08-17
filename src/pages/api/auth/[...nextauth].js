be926abed7c52cce9973979f5cd9aa0e3bc27df2
aidan.newell <aidan.newell@river.com>
2023-08-17 07:43:51 -0500

    added callbacks and wrapped session provider in _app

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 export const authOptions = {
@@ -7,7 +8,24 @@ export const authOptions = {
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
-    // ...add more providers here
   ],
+  callbacks: {
+    async session({ session }) {
+      console.log("session", session);
+      const user = await axios.post("http://localhost:3000/api/users", {
+        username: session.user.name,
+      });
+
+      if (user.status === 200) {
+        session.user = user.data.exists;
+        return session;
+      } else if (user.status === 201) {
+        session.user = user.data;
+        return session;
+      } else {
+        return session;
+      }
+    },
+  },
 };
 export default NextAuth(authOptions);
