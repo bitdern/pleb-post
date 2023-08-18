b991862bbc28e0080119e49dbc3d418d9086f48c
aidan.newell <aidan.newell@river.com>
2023-08-18 08:08:45 -0500

    testing gh auth

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,7 +3,7 @@ import { SessionProvider } from "next-auth/react";
 
 export default function App({ Component, pageProps }) {
   return (
-    <SessionProvider session={session}>
+    <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
     </SessionProvider>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,23 @@
-import Image from "next/image";
+import { useEffect } from "react";
+import { useSession, signIn, signOut } from "next-auth/react";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const { data: session, status } = useSession();
+
+  useEffect(() => {
+    console.log(session);
+  }, [session]);
+
   return (
     <main>
-      <div>Hello World</div>
+      <h1>Hello World</h1>
+      <h1>{status}</h1>
+
+      <button onClick={() => signIn()}>login</button>
+      <button onClick={() => signOut()}>logout</button>
     </main>
   );
 }
