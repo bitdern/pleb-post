a01b66cc53862e7e23492acece35cb02e05e74b8
aidan.newell <aidan.newell@river.com>
2023-08-26 09:59:09 -0500

    added a post list component and styles module without any styles lol

--- src/components/posts/postsList.js
+++ src/components/posts/postsList.js
@@ -1,9 +1,32 @@
-import React from "react";
-import { useSession, signIn, signOut } from "next-auth/react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import styles from "./styles.module.css";
 
-const postsList = () => {
-  return;
+const PostsList = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/api/posts")
+      .then((res) => {
+        setPosts(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <div className={styles.posList}>
+      {posts.map((post) => {
+        <div className={styles.post} key={post._id}>
+          <h3>{post.title}</h3>
+          <p>{post.description}</p>
+          <span>{post.author}</span>
+        </div>;
+      })}
+    </div>
+  );
 };
 
-export default postsList;
+export default PostsList;
